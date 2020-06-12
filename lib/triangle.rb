ca7e59b2173813ba683bdf5ac1373d45c3a447dd
Maloochow <luyao.mly@gmail.com>
2020-06-12 19:04:13 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+attr_accessor :sides
+
+def initialize(a, b, c)
+  @sides = [a, b, c]
+end
+
+def kind
+  if side_0? || side_invalid?
+    raise TriangleError
+  elsif sides[0] == sides[1] && sides[0] == sides[2]
+    :equilateral
+  elsif isosceles?
+    :isosceles
+  else
+    :scalene
+  end
+end
+
+def sides_combo
+  sides.combination(2).to_a
+end
+
+def side_invalid?
+  sides_combo.detect {|i| i[0] + i[1] <= (sides - [i[0], i[1]]).join.to_i}
+end
+
+def side_0?
+  sides.detect {|a| a <= 0 }
+end
+
+
+def isosceles?
+  sides_combo.detect {|combo| combo[0] == combo[1]}
+end
+
+class TriangleError < StandardError
+end
+
 end
